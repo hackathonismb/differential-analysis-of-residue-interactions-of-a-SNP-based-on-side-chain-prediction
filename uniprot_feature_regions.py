55081c16c6fa60284cd4fe7660269cf9736fc066
msarrias <marina.sarrias@math.su.se>
2022-07-15 10:22:37 -0500

    added some documentation

--- uniprot_feature_regions.py
+++ uniprot_feature_regions.py
@@ -3,6 +3,7 @@ import requests
 import json
 import argparse
 
+
 def argument_parser():
     parser = argparse.ArgumentParser()
     required_arguments = parser.add_argument_group("Required Arguments")
@@ -20,13 +21,23 @@ def argument_parser():
     return args
 
 
-def query_UniProt(uniprotkb):
-    # Query UniProt through the REST interface
-    API_URL = f'https://rest.uniprot.org/uniprot/{uniprotkb}'
-    return requests.get(API_URL).json()
+def query_uniprot(uniprotkb):
+    """
+    Queries UniProt using a REST interface
+    :param uniprotkb: uniprot id
+    :returns: dictionary
+    """
+    api_url = f'https://rest.uniprot.org/uniprot/{uniprotkb}'
+    return requests.get(api_url).json()
 
 
 def retrieve_feature_regions(data):
+    """
+    Given an uniprot id creates a nested dictionary with the coord
+    of the residues/regions of interest.
+    :param data: dictionary
+    :returns: dictionary
+    """
     features_dict = {}
     for feature in data['features']:
         if feature['type'] in ['Binding site',
@@ -42,15 +53,11 @@ def retrieve_feature_regions(data):
             start = feature['location']['start']['value']
             end = feature['location']['end']['value']
             if start != end:
-                if feature['type'] == 'Disulfide bond':
-                    coord = (start, end)
-                else:
-                    coord = [start, end]
-                
+                coord = [start, end]
                 if coord not in features_dict[feature['type']]:
                     features_dict[feature['type']].append(coord)
             else:
-                # those annotations with a single residue are set to have
+                # annotations with a single residue are set to have
                 # the same "start" and "end"
                 if start not in features_dict[feature['type']]:
                     features_dict[feature['type']].append(start)
@@ -58,36 +65,44 @@ def retrieve_feature_regions(data):
 
 
 def main():
+    """
+    'uniprot_feature_regions' takes as input two arguments:
+    (i) id, (ii) database (options are: 'uniprot' or 'pdb')
+    and returns a json file containing the feature regions of
+    each uniprot id.
+    """
     args = argument_parser()
     with open('Combined_PTMs.json', "r") as f:
-        combined_PTMs = json.load(f) 
-    if args.db == 'uniprot':
-        data = query_UniProt(args.id)
-        if 'messages' in data:
-            raise ValueError(f"invalid uniprotKB: {args.id}")
-        features_dict = retrieve_feature_regions(data)
-        if args.id in combined_PTMs:
-            features_dict = {**features_dict, **combined_PTMs[args.id]}
-    elif args.db == 'pdb':
-        job_id = submit_id_mapping(from_db="PDB", to_db="UniProtKB", ids=[args.id])
-        if check_id_mapping_results_ready(job_id):
-            link = get_id_mapping_results_link(job_id)
-            results = get_id_mapping_results_search(link)
+        combined_ptms = json.load(f)
+    if args.db in ['uniprot', 'pdb']:
         features_dict = {}
-        for hit in results['results']:
-            id_ = hit['to']['primaryAccession']
-            feature_hits_dict = retrieve_feature_regions(hit['to'])
-            if id_ in combined_PTMs:
-                feature_hits_dict = {**feature_hits_dict, **combined_PTMs[id_]}
-            features_dict[hit['to']['primaryAccession']] =  feature_hits_dict 
+        if args.db == 'uniprot':
+            data = query_uniprot(args.id)
+            if 'messages' in data:
+                raise ValueError(f"invalid uniprotKB: {args.id}")
+            features_dict = retrieve_feature_regions(data)
+            if args.id in combined_ptms:
+                features_dict = {**features_dict, **combined_ptms[args.id]}
+        elif args.db == 'pdb':
+            job_id = submit_id_mapping(from_db="PDB", to_db="UniProtKB", ids=[args.id])
+            if check_id_mapping_results_ready(job_id):
+                link = get_id_mapping_results_link(job_id)
+                results = get_id_mapping_results_search(link)
+                # a pdb can map to many uniprot ids (one per specie)
+                for hit in results['results']:
+                    id_ = hit['to']['primaryAccession']
+                    feature_hits_dict = retrieve_feature_regions(hit['to'])
+                    if id_ in combined_ptms:
+                        feature_hits_dict = {**feature_hits_dict, **combined_ptms[id_]}
+                    features_dict[hit['to']['primaryAccession']] = feature_hits_dict
+        if args.output is not None and '.json' in args.output:
+            filename = args.output
+        else:
+            filename = args.id + '_regions.json'
+        with open(filename, "w") as f:
+            f.write(json.dumps(features_dict))
     else:
         raise ValueError(f"invalid db {args.db}. Choose between uniprot and pdb")
-    if args.output != None and '.json' in args.output:
-        filename = args.output
-    else: 
-        filename = args.id + '_regions.json'
-    with open(filename, "w") as f:
-        f.write(json.dumps(features_dict))
 
 
 if __name__ == "__main__":
