fb738103738e107c1125a6505b066d7a4ee0522e
msarrias <marina.sarrias@math.su.se>
2022-07-14 21:13:56 -0500

    script for generating the combined PTMs .json file

---
+++ generate_combined_PTMs.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+with open('Combined_PTMs.csv', newline='') as csvfile:
+    data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    combined_ptms = {}
+next(data)
+for row in data:
+    uniprotID, _, PTMpos, PTMs, _ = row[0].rsplit(',')
+    if uniprotID not in combined_ptms:
+        combined_ptms[uniprotID] = {}
+    if PTMs not in combined_ptms[uniprotID]:
+        combined_ptms[uniprotID][PTMs] = []
+    combined_ptms[uniprotID][PTMs].append(int(PTMpos))
+with open('Combined_PTMs.json', "w") as f:
+    f.write(json.dumps(combined_ptms))        
