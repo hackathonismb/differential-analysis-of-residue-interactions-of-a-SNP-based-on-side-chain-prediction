d1dc9dacec398803b698c067507da7a4bb8c3b0e
msarrias <marina.sarrias@math.su.se>
2022-07-14 21:16:27 -0500

    fixed indentation

--- generate_combined_PTMs.py
+++ generate_combined_PTMs.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     with open('Combined_PTMs.csv', newline='') as csvfile:
         data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        combined_ptms = {}
+    combined_ptms = {}
     next(data)
     for row in data:
         uniprotID, _, PTMpos, PTMs, _ = row[0].rsplit(',')
