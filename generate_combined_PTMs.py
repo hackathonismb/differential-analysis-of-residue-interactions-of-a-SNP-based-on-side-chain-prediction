b3eb524a1987aba1d408c7b1ed002add8ded5918
Zachery Mielko <33356434+zmielko@users.noreply.github.com>
2022-07-21 16:34:46 -0400

    Input file and bug fix
    generate_combined_PTMs.py returns an error for I/O operations.
    
    ##Command##
    python generate_combined_PTMs.py
    
    ##Input File##
    
    Combined_PTMs.csv
    
    #head Combined_PTMs.csv#
    
    UniprotId,PTMaa,PTMpos,PTMs,Metadata
    P31946,K,5,Acetylation,https://www.phosphosite.org/siteAction.action?id=33347662
    Q9CQV8,K,5,Acetylation,https://www.phosphosite.org/siteAction.action?id=33347662
    P35213,K,5,Acetylation,https://www.phosphosite.org/siteAction.action?id=33347662
    P35213,K,11,Acetylation,https://www.phosphosite.org/siteAction.action?id=36313496
    P31946,K,13,Acetylation,https://www.phosphosite.org/siteAction.action?id=36297549
    P35213,K,13,Acetylation,https://www.phosphosite.org/siteAction.action?id=36297549
    P31946,K,51,Acetylation,https://www.phosphosite.org/siteAction.action?id=474928
    Q9CQV8,K,51,Acetylation,https://www.phosphosite.org/siteAction.action?id=474928
    P35213,K,51,Acetylation,https://www.phosphosite.org/siteAction.action?id=474928
    
    ##Error##
    Traceback (most recent call last):
      File "/home/zmielko/Documents/Hackathon_ISMB_2022/GitHub/generate_combined_PTMs.py", line 8, in <module>
        next(data)
    ValueError: I/O operation on closed file.
    
    ##Solution##
    I placed all of the actions for using the csv reader object within the while loop and
    moved the dictionary being populated outside of it at the start.

--- generate_combined_PTMs.py
+++ generate_combined_PTMs.py
@@ -2,16 +2,16 @@ import csv
 import json
 
 if __name__ == "__main__":
+    combined_ptms = {}
     with open('Combined_PTMs.csv', newline='') as csvfile:
         data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    combined_ptms = {}
-    next(data)
-    for row in data:
-        uniprotID, _, PTMpos, PTMs, _ = row[0].rsplit(',')
-        if uniprotID not in combined_ptms:
-            combined_ptms[uniprotID] = {}
-        if PTMs not in combined_ptms[uniprotID]:
-            combined_ptms[uniprotID][PTMs] = []
-        combined_ptms[uniprotID][PTMs].append(int(PTMpos))
+        next(data)
+        for row in data:
+            uniprotID, _, PTMpos, PTMs, _ = row[0].rsplit(',')
+            if uniprotID not in combined_ptms:
+                combined_ptms[uniprotID] = {}
+            if PTMs not in combined_ptms[uniprotID]:
+                combined_ptms[uniprotID][PTMs] = []
+            combined_ptms[uniprotID][PTMs].append(int(PTMpos))
     with open('Combined_PTMs.json', "w") as f:
-        f.write(json.dumps(combined_ptms))        
+        f.write(json.dumps(combined_ptms))
