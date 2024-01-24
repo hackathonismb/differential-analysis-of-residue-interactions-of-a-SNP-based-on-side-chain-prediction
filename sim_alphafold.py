03e3ff04d94d4b1e87701c55b708d75862ab884e
msarrias <marina.sarrias@math.su.se>
2024-01-24 11:14:39 +0100

    added argparser and refactored

--- sim_alphafold.py
+++ sim_alphafold.py
@@ -1,27 +1,67 @@
 import os
 
-PROJ_DIR = '/proj/snic2022-22-771/users/x_maher'
-DATA_DIR = f'{PROJ_DIR}/data/test_fasta'
-WORKING_DIR = f'{PROJ_DIR}/ALPHAFOLD'
-DUMP_DIR = f'{WORKING_DIR}/Benchmark_ALPHAFOLD'
-filenames_list = [filename for filename in os.listdir(DATA_DIR) if '.fasta' in filename]
-identifiers_list =  [filename.rsplit('.fasta')[0] for filename in filenames_list]
-args_list = [identifier for identifier in identifiers_list if not os.path.exists(f'{DUMP_DIR}/{identifier}')]  
 
-if args_list:
-    for identifier_filename in filenames_list:
-        identifier = identifier_filename.rsplit('.fasta')[0]
-        sim_direct = f'{DUMP_DIR}/{identifier}'
-        file_path = f'{DATA_DIR}/{identifier_filename}'
-        alphafold_call = ['run_singularity_af',
-                          f'--fasta_paths={file_path}',
-                          f'--output_dir={sim_direct}',
-                          '--model_preset=monomer', 
-                          '--db_preset=full_dbs',
-                          '--bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt',
-                          '--pdb70_database_path=/data/pdb70/pdb70',
-                          '--uniclust30_database_path=/data/uniclust30/uniclust30_2018_08/uniclust30_2018_08',
-                          '--max_template_date=2020-05-14',
-                          '--use_gpu_relax=True']
-        os.system(' '.join(alphafold_call)) 
-    
+def argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--fasta_directory',
+        type=str,
+        help='directory with the fasta files to process',
+    )
+    parser.add_argument(
+        '--output_directory',
+        type=str,
+        help='output directory',
+    )
+    parser.add_argument(
+        '--alphafold_dataset',
+        type=str,
+        help='alphafold dataset',
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = argument_parser()
+    fasta_directory = args.fasta_directory
+    output_directory = args.output_directory
+    alphafold_dataset = args.alphafold_dataset
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+    filenames_list = [
+        filename
+        for filename in os.listdir(fasta_directory)
+        if '.fasta' in filename
+    ]
+    identifiers_list = [
+        filename.rsplit('.fasta')[0]
+        for filename in filenames_list
+    ]
+    args_list = [
+        identifier
+        for identifier in identifiers_list
+        if not os.path.exists(f'{output_directory}/{identifier}')
+    ]
+    if args_list:
+        for identifier_filename in filenames_list:
+            identifier = identifier_filename.rsplit('.fasta')[0]
+            input_path = os.path.join(output_directory, identifier)
+            out_path = os.path.join(fasta_directory, identifier_filename)
+            # You need to adapt the following command to your
+            # alphafold installation, the following is tailored for running
+            # at uppmax
+            alphafold_command = [
+                'bash',
+                'run_alphafold.sh',
+                f'-d {alphafold_dataset}',
+                f'--fasta_paths={input_path}',
+                f'--output_dir={out_path}',
+                '--model_preset=monomer',
+                '--db_preset=full_dbs'
+            ]
+            os.system(' '.join(alphafold_command), shell=True)
+
+
+if __name__ == "__main__":
+    main()
